3c4f2126d181b4568fcdedc5206c9b0856a0f78e
Mohak Sharma <mohak327@gmail.com>
2021-03-09 14:58:38 +0530

    npx cleanup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="a  pp">
+      <h1>Let's build a LinkedIn clone.</h1>
+    </div>
+  );
+}
+
+export default App;
